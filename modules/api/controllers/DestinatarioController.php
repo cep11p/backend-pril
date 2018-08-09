2cfa9cb907cd8298bf5922ea5a6e90849e4d6892
cep11p <cep11p@gmail.com>
2018-08-09 16:58:48 -0300

    https://tree.taiga.io/project/cep11p-pril/us/65?no-milestone=1 https://tree.taiga.io/project/cep11p-pril/us/148?no-milestone=1
    Se realizan las funciones adecuadas para registrar una destinatario con datos persona, es decir realizando una interoperabilidad con el sistemaRegistral

--- modules/api/controllers/DestinatarioController.php
+++ modules/api/controllers/DestinatarioController.php
@@ -3,6 +3,10 @@ namespace app\modules\api\controllers;
 
 use yii\rest\ActiveController;
 use yii\web\Response;
+use Yii;
+use yii\base\Exception;
+/**Models**/
+use app\models\Destinatario;
 
 class DestinatarioController extends ActiveController{
     
@@ -45,5 +49,51 @@ class DestinatarioController extends ActiveController{
 
 
         return $behaviors;
+    }
+    
+    public function actions()
+    {
+        $actions = parent::actions();
+        unset($actions['create']);
+        return $actions;
+    
+    }
+    
+    /**
+     * Se crea un Destinatario y se vincula con una Persona()
+     * @return array Un array con datos
+     * @throws \yii\web\HttpException
+     */
+    public function actionCreate()
+    {
+        $resultado['message']='Se guarda un Destinatario';
+        $param = Yii::$app->request->post();
+        $transaction = Yii::$app->db->beginTransaction();
+        try {
+            
+            $model = new Destinatario();
+            $model->setAttributes($param);
+            //Registrar y validar personaid
+            
+            if(!$model->save()){
+                $arrayErrors['destinatario']=$model->getErrors();
+                $arrayErrors['tab']='destinatario';                
+                throw new Exception(json_encode($arrayErrors));
+            }
+            
+            $transaction->commit();
+            
+            $resultado['success']=true;
+            $resultado['data']['id']=$model->id;
+            
+            return  $resultado;
+           
+        }catch (Exception $exc) {
+            //echo $exc->getTraceAsString();
+            $transaction->rollBack();
+            $mensaje =$exc->getMessage();
+            throw new \yii\web\HttpException(500, $mensaje);
+        }
+
     }
 }
