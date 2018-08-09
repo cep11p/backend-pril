afdae2fc723b9652c6408d2669fafcf0d9c95840
cep11p <cep11p@gmail.com>
2018-08-09 16:59:55 -0300

    Se agregan un ejemplos de Mariano para realizar un testeo con mock

--- tests/unit/models/UserTest.php
+++ tests/unit/models/UserTest.php
@@ -33,6 +33,24 @@ class UserTest extends \Codeception\Test\Unit
      */
     public function testValidateUser($user)
     {
+        
+        $manager = $this->getMockBuilder('yii\rbac\PhpManager')
+        ->setMethods(['checkAccess'])
+        ->getMock();        
+        $manager->expects($this->any())->method('checkAccess')->willReturn(TRUE);        
+        \Yii::$app->set('authManager', $manager);
+        
+        // cargar fixture
+        $logger = $this->getMockBuilder('yii\\log\\Logger')
+            ->setMethods(['log'])
+            ->getMock();        
+         \Yii::setLogger($logger);
+        $logger->expects($this->any())->method('log')->willReturnCallback(function() use (&$arrayLogs){
+            $parametros = func_get_args();
+            $arrayLogs[] = $parametros;
+        });
+
+        //die(\yii\helpers\VarDumper::dumpAsString($arrayLogs));
         $user = User::findByUsername('admin');
         expect_that($user->validateAuthKey('test100key'));
         expect_not($user->validateAuthKey('test102key'));
