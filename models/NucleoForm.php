b98b41e9072cf43f2a4547bdd7097f8a5a1f094b
cep11p <cep11p@gmail.com>
2018-09-27 10:12:43 -0300

    TG-266
    se modulariza todo el funcionamiento que tiene que ver con nucleo
    
    https://tree.taiga.io/project/cep11p-pril/task/266

--- models/NucleoForm.php
+++ models/NucleoForm.php
@@ -20,8 +20,43 @@ class NucleoForm extends Model
         return [
             [['nombre'], 'required'],
             [['jefeid', 'hogarid'], 'integer'],
-            [['nombre'], 'string', 'max' => 100]
+            [['nombre'], 'string', 'max' => 100],
+            ['id','existeNucleoEnSistemaRegistral']
         ];
+    }
+    
+    public function existeNucleoEnSistemaRegistral(){
+        $resultado = null;
+        $response = \Yii::$app->registral->buscarNucleoPorId($this->id);
+        
+        if(isset($response['success']) && $response['success']==true){
+
+            if(count($response['resultado'])==0){            
+                $this->addError("id","El nucleo con el id $this->id no existe!");
+            }
+        }
+    }
+
+        /**
+     * Se busca un nucleo usando un criterio de busquedad que es armado mediantes los atributos recibido de $param
+     * @param array $param son atributos asociativos relevante a la busquedad
+     * @return string
+     */
+    public function buscarNucleoEnSistemaRegistral($param) {
+        
+        $resultado = null;
+        $response = \Yii::$app->registral->buscarNucleo($param);
+        
+        if(isset($response['success']) && $response['success']==true){
+
+            if(count($response['resultado'])>0){            
+                foreach ($response['resultado'] as $modeloEncontrado){
+                    $resultado = $modeloEncontrado;
+                }
+            }
+        }
+        
+        return $resultado;
     }
     
     
