7101ddfd62805ed99a23903e82495a82ce93db86
cep11p <cep11p@gmail.com>
2018-10-10 11:19:49 -0300

    TG-285
    Ahora tambien se muestra el nombre de la localidad de la persona
    
    https://tree.taiga.io/project/cep11p-pril/task/285

--- models/PersonaForm.php
+++ models/PersonaForm.php
@@ -110,32 +110,13 @@ class PersonaForm extends Model
         if(isset($response['estado']) && $response['estado']==true){
             $personaArray = $response['resultado'][0];
             
-            #Preparamos los atributos de lugar
+            #Sacamos el parametro lugar que para pril es irrelevante
             if(isset($personaArray['hogar'])){
-                $lugarid = $personaArray['hogar']['lugarid'];
-                $lugarArray = $this->getLugar($lugarid);
-                
                 unset($personaArray['hogar']);
-                $personaArray['lugar'] = $lugarArray;
             }
         }
         
         return $personaArray;
-    }
-    
-    /**
-     * Esta funcion busca un lugar en el sistema lugar
-     * @param int $id
-     * @return array con atributos de lugar, en caso que no exista se devuelve un Null;
-     */
-    protected function getLugar($lugarid){
-        $response = \Yii::$app->lugar->buscarLugarPorId($lugarid);
-        $resultado = null;
-        if(isset($response['success']) && $response['success']==true){
-            $resultado = $response['resultado'][0];
-        }
-        
-        return $resultado;
     }
 
     /**
@@ -187,126 +168,8 @@ class PersonaForm extends Model
             }
         }
     }
-//    public function existeEnRegistral(){
-//        $resultado = false;
-//        
-//        $response = \Yii::$app->registral->buscarPersonaPorId($this->id);       
-//        
-//        if(isset($response['estado']) && $response['estado']==true){
-//            $resultado = true;
-//        }
-//        
-//        return $resultado;
-//    }
     
-    /**
-     * 
-     * @param array $param
-     * @throws Exception
-     */
-//    public function agregarHogar($param) {
-//        /*** Nucleo y su Hogar ***/ //el param['nucleo']['hogar'] viene con nucleos[] (vacio)
-//        if(isset($param['hogar']) && count($param['hogar'])>1){
-//            //Instanceamos el hogar a modificar
-//            if(isset($param['hogar']['id']) && !empty($param['hogar']['id'])){
-//                $hogar = Hogar::findOne(['id'=>$param['hogar']['id']]);
-//                $hogar->setAttributes($param['hogar']);
-//                
-//            }else{
-//            //Creamos el hogar nuevo
-//                $hogar = new Hogar();
-//                $hogar->setAttributes($param['hogar']);
-//            }
-//            
-//            if(!$hogar->save()){
-//                $arrayErrors['hogar']=$hogar->getErrors();
-//                $arrayErrors['tab']='hogar';
-//                $resultado['success']=false;
-//                throw new Exception(json_encode($arrayErrors));
-//            }
-//            
-//            /*** Nucleo ***/
-//            /******************************/
-//            
-//            //Si ya existe el nucleo
-//            if(isset($param['nucleo']['id']) && !empty($param['nucleo']['id'])){
-//                //si id no es un entero
-//                if(!is_int($param['nucleo']['id'])){
-//                    $msj = 'El id nucleo es incorrecto';
-//                    throw new Exception($msj);
-//                }
-//                
-//                $nucleo = Nucleo::findOne(['id'=>$param['nucleo']['id']]);
-//                
-//                if($nucleo==null){
-//                    $msj = 'El nucleo no existe!!';
-//                    throw new Exception($msj);
-//                }
-//                
-//                if(!in_array($nucleo, $hogar->nucleos)){
-//                    $msj = 'El nucleo no pertenece al hogar';
-//                    throw new Exception($msj);
-//                }
-//                
-//            }else{
-//                $nucleo = new Nucleo();
-//                $nucleo->hogarid = $hogar->id;
-//
-//                if(isset($param['nucleo']['nombre']) && !empty($param['nucleo']['nombre'])){
-//                    $nucleo->nombre = $param['nucleo']['nombre'];
-//                }else{
-//                    $this->addError('nombre', 'Se debe crear o seleccionar un Nucleo');
-//                }
-//                
-//                if(!$nucleo->save()){
-//                    $arrayErrors['nucleo']=$nucleo->getErrors();
-//                    $arrayErrors['tab']='nucleo';
-//                    $resultado['success']=false;
-//                    throw new Exception(json_encode($arrayErrors));
-//                }
-//
-//            }
-//
-//            //instanciamos el nucleo a la persona actual
-//            $this->nucleoid = $nucleo->id;
-//            
-//        }
-//    }
-    
-    /**
-     * Buscamos si existe la persona con el nro_documento
-     * @return bool
-     */
-    public function existePersonaConNroDocumento()
-    {
-        
-        $per = Persona::findOne(["nro_documento"=>$this->nro_documento]);
-        $resultado = true;
-        
-        if($per==NUll){
-            $resultado = false;
-        }
-        
-        return $resultado;
-    }
-    
-    public function getHogar()
-    {
-        $nucleo = Nucleo::findOne([['id'=>$this->nucleoid]]);
-        $arrayHogar = array();
-        if($nucleo!=NULL){
-            
-            $hogar = Hogar::findOne(['id'=>  $nucleo->hogarid]);
-            
-            if($hogar!=NULL){
-                $arrayHogar = $hogar;
-            }
-            
-        }
-        
-        return $arrayHogar;
-    }
-    
+     
     
     
     
