549eceae4fb5103ab804d5d310045f36be48d8c1
cep11p <cep11p@gmail.com>
2018-11-12 12:54:38 -0300

    TG-333
    se crean mas fixtures
    
    https://tree.taiga.io/project/cep11p-pril/us/333?no-milestone=1

--- tests/_data/ambiente_trabajo.php
+++ tests/_data/ambiente_trabajo.php
@@ -12,6 +12,50 @@ return [
 	    "personaid"=> 1,
             "lugarid"=>1
    ],
+    "ambiente_trabajo2"=>[
+            "nombre"=> "Panaderia Boomble",
+	    "calificacion"=> 7,
+	    "legajo"=> "asb123/1",
+	    "observacion"=>"es una empresa que realiza actividades de panaderia y pasteleria",
+	    "cuit"=>"20123456789",
+	    "actividad"=> "Vende facturas, tortas y variedades de panes",
+	    "tipo_ambiente_trabajoid"=> 1,
+	    "personaid"=> 2,
+            "lugarid"=>2
+   ],
+    "ambiente_trabajo3"=>[
+            "nombre"=> "Saturno Hogar",
+	    "calificacion"=> 7,
+	    "legajo"=> "asb123/2",
+	    "observacion"=>"es una empresa que vende electrodomestico",
+	    "cuit"=>"20123456789",
+	    "actividad"=> "es una empresa que vende electrodomestico",
+	    "tipo_ambiente_trabajoid"=> 2,
+	    "personaid"=> 3,
+            "lugarid"=>3
+   ],
+    "ambiente_trabajo4"=>[
+            "nombre"=> "Heladeria Sei Tu",
+	    "calificacion"=> 7,
+	    "legajo"=> "asb123/3",
+	    "observacion"=>"es una empresa que vende Helados",
+	    "cuit"=>"20123456789",
+	    "actividad"=> "es una empresa que vende Helados",
+	    "tipo_ambiente_trabajoid"=> 2,
+	    "personaid"=> 4,
+            "lugarid"=>4
+   ],
+    "ambiente_trabajo5"=>[
+            "nombre"=> "Open Sport",
+	    "calificacion"=> 7,
+	    "legajo"=> "asb123/4",
+	    "observacion"=>"",
+	    "cuit"=>"20123456789",
+	    "actividad"=> "es una empresa que vende Calsados",
+	    "tipo_ambiente_trabajoid"=> 2,
+	    "personaid"=>5,
+            "lugarid"=>5
+   ],
     
     
 ];
