adfc86c143e6e0b17f13bf728c4df6dc2554a24f
cep11p <cep11p@gmail.com>
2018-11-26 13:42:34 -0300

    TG-369
    se documenta la funcion buscar-por-documento
    
    https://tree.taiga.io/project/cep11p-pril/us/369?no-milestone=1

---
+++ documentacionJson/Persona.php
@@ -0,0 +1,39 @@
+<?php
+
+/**** obtener lista de Personas ***
+@url ejemplo http://api.pril.local/api/personas?nombre=lorena&nro_documento=36849868
+@Method GET
+
+{
+    
+}
+**/
+
+/**** obtener Persona por nro_documento ***
+@url ejemplo http://api.pril.local/api/personas/buscar-por-documento/29800100
+@Method GET
+{
+    "success": true,
+    "resultado": [
+        {
+            "id": 1,
+            "nombre": "Romina Belen",
+            "apellido": "Rodríguez",
+            "apodo": "rominochi",
+            "nro_documento": "29800100",
+            "fecha_nacimiento": "1980-12-12",
+            "estado_civilid": 1,
+            "telefono": "2920430690",
+            "celular": "2920412127",
+            "sexoid": 2,
+            "tipo_documentoid": 1,
+            "nucleoid": 1,
+            "situacion_laboralid": 1,
+            "generoid": 1,
+            "email": null,
+            "cuil": "21298001007",
+            ...,
+        }
+    ]
+}
+**/
