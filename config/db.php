8cd3cf012486c845971ed2d5a799f307c107d0b7
cep11p <cep11p@gmail.com>
2019-01-04 10:55:05 -0300

    se reorganiza el proyecto con docker-compose

--- config/db.php
+++ config/db.php
@@ -2,7 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=mimysql;dbname=pril',
+    //'dsn' => 'mysql:host=mimysql;dbname=pril',
+    'dsn' => 'mysql:host='.getenv('DB_HOST').';dbname='.getenv('DB_NAME'),
     'username' => 'root',
     'password' => 'root',
     'charset' => 'utf8',
