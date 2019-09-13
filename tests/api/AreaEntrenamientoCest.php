3f981c411131051b09a259ac84e26718372d5bc1
cep11p <cep11p@gmail.com>
2019-09-13 13:31:12 -0300

    TG-361 Se testea el listado de area de entrenamiento https://tree.taiga.io/project/cep11p-pril/us/361?no-milestone=1

--- tests/api/AreaEntrenamientoCest.php
+++ tests/api/AreaEntrenamientoCest.php
@@ -60,202 +60,113 @@ class AreaEntrenamientoCest
         $I->seeResponseCodeIs(200);
         
     }
-    /**
-     * Si los campos de lugar estan vacios se debe notificar
-     * @param ApiTester $I
-     */
-//    public function agregarUnAmbienteTrabajoConCamposDeLugarVacio(ApiTester $I)
-//    {
-//        $I->wantTo('Agregar Un Ambiente de trabajo con los Campos De Lugar Vacio');
-//        $param=[
-//                "nombre"=> "Panaderia San Fernando",
-//                "calificacion"=> 7,
-//                "legajo"=> "asb123/7",
-//                "observacion"=>"es una empresa que realiza actividades de panaderia y pasteleria",
-//                "cuit"=>"20123456789",
-//                "actividad"=> "Vende facturas, tortas y variedades de panes",
-//                "tipo_ambiente_trabajoid"=> 1,
-//                "persona"=>[
-//                    "nombre"=> "Diego",
-//                    "apellido"=> "Matinez",
-//                    "nro_documento"=> "27890098",
-//                    "fecha_nacimiento"=>"1980-12-12",
-//                    "apodo"=>"rominochi",
-//                    "telefono"=> "2920430690",
-//                    "celular"=> "2920412127",
-//                    "situacion_laboralid"=> 1,
-//                    "estado_civilid"=> 1,
-//                    "sexoid"=> 2,
-//                    "tipo_documentoid"=> 1,
-//                    "generoid"=> 1,
-//                    "email"=>"algo@correo.com.ar",
-//                    "cuil"=>"20367655678"
-//
-//                ]   
-//            ];
-//        
-//        $I->sendPOST('/api/ambiente-trabajos', $param);
-//        
-//        $I->seeResponseContainsJson([
-//            'message' => '{"lugar":{"calle":["Calle no puede estar vac\u00edo."],"altura":["Altura no puede estar vac\u00edo."],"localidadid":["Localidadid no puede estar vac\u00edo."]}}',
-//        ]);
-//        
-//        $I->seeResponseCodeIs(500);
-//        
-//    }
-    /**
-     * Si localidadid no existe, deberia ser notificado
-     * @param ApiTester $I
-     */
-//    public function agregarUnAmbienteTrabajoConLocalidadNoExistente(ApiTester $I)
-//    {
-//        $I->wantTo('Agregar Un Ambiente de trabajo con localidad no existente');
-//        $param=[
-//            "nombre"=> "Panaderia San Fernando",
-//            "calificacion"=> 7,
-//            "legajo"=> "asb123/7",
-//            "observacion"=>"es una empresa que realiza actividades de panaderia y pasteleria",
-//            "cuit"=>"20123456789",
-//            "actividad"=> "Vende facturas, tortas y variedades de panes",
-//            "tipo_ambiente_trabajoid"=> 1,
-//            "lugar"=>[
-//                "calle"=>"Mata Negra",
-//                "altura"=>"123",
-//                "localidadid"=>0
-//            ],
-//            "persona"=>[
-//                "nombre"=> "Diego",
-//                "apellido"=> "Matinez",
-//                "nro_documento"=> "27890098",
-//                "fecha_nacimiento"=>"1980-12-12",
-//                "apodo"=>"rominochi",
-//                "telefono"=> "2920430690",
-//                "celular"=> "2920412127",
-//                "situacion_laboralid"=> 1,
-//                "estado_civilid"=> 1,
-//                "sexoid"=> 2,
-//                "tipo_documentoid"=> 1,
-//                "generoid"=> 1,
-//                "email"=>"algo@correo.com.ar",
-//                "cuil"=>"20367655678"
-//            ]
-//        ];
-//        
-//        $I->sendPOST('/api/ambiente-trabajos', $param);
-//        
-//        $I->seeResponseContainsJson([
-//            'message' => '{"lugar":{"id":["La localidad con el id  no existe!"]}}',
-//        ]);
-//        
-//        $I->seeResponseCodeIs(500);
-//        
-//    }
     
-//    public function agregarUnAmbienteTrabajoConLugaridIncorrecto(ApiTester $I)
-//    {
-//        $I->wantTo('Agregar Un Ambiente de trabajo con lugarid incorrecto');
-//        $param=[
-//            "nombre"=> "Panaderia San Fernando",
-//            "calificacion"=> 7,
-//            "legajo"=> "asb123/7",
-//            "observacion"=>"es una empresa que realiza actividades de panaderia y pasteleria",
-//            "cuit"=>"20123456789",
-//            "actividad"=> "Vende facturas, tortas y variedades de panes",
-//            "tipo_ambiente_trabajoid"=> 1,
-//            "lugarid"=>0,
-//            "lugar"=>[
-//                "calle"=>"Mata Negra",
-//                "altura"=>"123",
-//                "localidadid"=>1
-//            ],
-//            "persona"=>[
-//                "nombre"=> "Diego",
-//                "apellido"=> "Matinez",
-//                "nro_documento"=> "27890098",
-//                "fecha_nacimiento"=>"1980-12-12",
-//                "apodo"=>"rominochi",
-//                "telefono"=> "2920430690",
-//                "celular"=> "2920412127",
-//                "situacion_laboralid"=> 1,
-//                "estado_civilid"=> 1,
-//                "sexoid"=> 2,
-//                "tipo_documentoid"=> 1,
-//                "generoid"=> 1,
-//                "email"=>"algo@correo.com.ar",
-//                "cuil"=>"20367655678"
-//
-//            ]
-//        ];
-//        
-//        $I->sendPOST('/api/ambiente-trabajos', $param);
-//        
-//        $I->seeResponseContainsJson([
-//            'message' => '{"ambiente_trabajo":{"lugarid":["No se pudo registrar el Lugar correctamente en el Sistema Lugar."]}}',
-//        ]);
-//        
-//        $I->seeResponseCodeIs(500);
-//    
-//    }
+    public function listarAreaEntrenamiento(ApiTester $I)
+    {
+        $I->wantTo('listar Area de entrenamiento');
+        
+        $I->sendGET('/api/area-entrenamientos');
+        
+        $I->seeResponseContainsJson([
+            "total_filtrado"=> 5,
+            "success"=> true,
+            "resultado"=> [
+                [
+                    "id"=> 1,
+                    "destinatarioid"=> 1,
+                    "ofertaid"=> 4,
+                    "fecha_inicial"=> "2018-12-12 00:00:00",
+                    "fecha_final"=> null,
+                    "tarea"=> "una tarea fixture",
+                    "plan_nombre"=> "Plan A",
+                    "plan_monto"=> "2000",
+                    "plan_hora_semanal"=> "10hs",
+                    "estado"=> "Vigente (estatico)",
+                    "oferta"=> [
+                        "id"=> 4,
+                        "ambiente_trabajoid"=> 2,
+                        "ambiente_trabajo"=> "Panaderia Boomble",
+                        "nombre_sucursal"=> "Sucursal Nº 1"
+                    ]
+                ],
+                [
+                    "id"=> 2,
+                    "destinatarioid"=> 2,
+                    "ofertaid"=> 3,
+                    "fecha_inicial"=> "2017-10-12 00:00:00",
+                    "fecha_final"=> null,
+                    "tarea"=> "una tarea fixture",
+                    "plan_nombre"=> "Plan A",
+                    "plan_monto"=> "2000",
+                    "plan_hora_semanal"=> "10hs",
+                    "estado"=> "Vigente (estatico)",
+                    "oferta"=> [
+                        "id"=> 3,
+                        "ambiente_trabajoid"=> 1,
+                        "ambiente_trabajo"=> "Panaderia San Fernando",
+                        "nombre_sucursal"=> "Sucursal Nº 2"
+                    ]
+                ],
+                [
+                    "id"=> 3,
+                    "destinatarioid"=> 3,
+                    "ofertaid"=> 2,
+                    "fecha_inicial"=> "2017-10-12 00:00:00",
+                    "fecha_final"=> null,
+                    "tarea"=> "una tarea fixture",
+                    "plan_nombre"=> "Plan A",
+                    "plan_monto"=> "2000",
+                    "plan_hora_semanal"=> "10hs",
+                    "estado"=> "Vigente (estatico)",
+                    "oferta"=> [
+                        "id"=> 2,
+                        "ambiente_trabajoid"=> 1,
+                        "ambiente_trabajo"=> "Panaderia San Fernando",
+                        "nombre_sucursal"=> "Sucursal Nº 2"
+                    ]
+                ],
+                [
+                    "id"=> 4,
+                    "destinatarioid"=> 4,
+                    "ofertaid"=> 1,
+                    "fecha_inicial"=> "2017-10-12 00:00:00",
+                    "fecha_final"=> null,
+                    "tarea"=> "una tarea fixture",
+                    "plan_nombre"=> "Plan A",
+                    "plan_monto"=> "2000",
+                    "plan_hora_semanal"=> "10hs",
+                    "estado"=> "Vigente (estatico)",
+                    "oferta"=> [
+                        "id"=> 1,
+                        "ambiente_trabajoid"=> 1,
+                        "ambiente_trabajo"=> "Panaderia San Fernando",
+                        "nombre_sucursal"=> "Sucursal Nº 1"
+                    ]
+                ],
+                [
+                    "id"=> 5,
+                    "destinatarioid"=> 2,
+                    "ofertaid"=> 1,
+                    "fecha_inicial"=> "2018-12-12 03:00:00",
+                    "fecha_final"=> null,
+                    "tarea"=> "una tarea",
+                    "plan_nombre"=> "Plan A",
+                    "plan_monto"=> "2000",
+                    "plan_hora_semanal"=> "10hs",
+                    "estado"=> "Vigente (estatico)",
+                    "oferta"=> [
+                        "id"=> 1,
+                        "ambiente_trabajoid"=> 1,
+                        "ambiente_trabajo"=> "Panaderia San Fernando",
+                        "nombre_sucursal"=> "Sucursal Nº 1"
+                    ]
+                ]
+            ]
+        ]);
+        
+        $I->seeResponseCodeIs(200);
+        
+    }
     
-//    public function agregarUnAmbienteTrabajo(ApiTester $I)
-//    {
-//        $I->wantTo('Agregar Un Ambiente');
-//        $param=[
-//            "nombre"=> "Panaderia San Fernando",
-//            "calificacion"=> 7,
-//            "legajo"=> "asb123/7",
-//            "observacion"=>"es una empresa que realiza actividades de panaderia y pasteleria",
-//            "cuit"=>"20123456789",
-//            "actividad"=> "Vende facturas, tortas y variedades de panes",
-//            "tipo_ambiente_trabajoid"=> 1,
-//            "lugar"=>[
-//                "calle"=>"Mata Negra",
-//                "altura"=>"123",
-//                "localidadid"=>1
-//            ],
-//            "persona"=>[
-//                "nombre"=> "Diego",
-//                "apellido"=> "Matinez",
-//                "nro_documento"=> "27890098",
-//                "fecha_nacimiento"=>"1980-12-12",
-//                "apodo"=>"rominochi",
-//                "telefono"=> "2920430690",
-//                "celular"=> "2920412127",
-//                "situacion_laboralid"=> 1,
-//                "estado_civilid"=> 1,
-//                "sexoid"=> 2,
-//                "tipo_documentoid"=> 1,
-//                "generoid"=> 1,
-//                "email"=>"algo@correo.com.ar",
-//                "cuil"=>"20367655678"
-//
-//            ]
-//        ];
-//        
-//        $I->sendPOST('/api/ambiente-trabajos', $param);
-//        $I->seeResponseContainsJson([
-//            'message' => 'Se guarda un Ambiente de Trabajo',
-//            'success' => true
-//        ]);
-//        
-//        $I->seeResponseCodeIs(200);
-//        
-//        //chequeamos lo guardado
-//        $model = AmbienteTrabajo::findOne(['legajo'=>'asb123/7']);
-//        $id = $model->id;
-//        $I->sendGET("/api/ambiente-trabajos/$id");
-//        $I->seeResponseContainsJson([
-//            'id' => $id,
-//            "nombre"=> "Panaderia San Fernando",
-//            "calificacion"=> 7,
-//            "legajo"=> "asb123/7",
-//            "observacion"=>"es una empresa que realiza actividades de panaderia y pasteleria",
-//            "cuit"=>"20123456789",
-//            "actividad"=> "Vende facturas, tortas y variedades de panes",
-//            "tipo_ambiente_trabajoid"=> 1,
-//        ]);
-//        $I->seeResponseCodeIs(200);
-//    
-//    }
     
 }
