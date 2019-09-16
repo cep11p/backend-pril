a0f2acf45970c33e1c8209d3cb8630586ddd84d7
cep11p <cep11p@gmail.com>
2019-09-16 13:51:44 -0300

    TG-340 Se testea la lista de ofertas https://tree.taiga.io/project/cep11p-pril/us/340?no-milestone=1

--- tests/api/OfertaCest.php
+++ tests/api/OfertaCest.php
@@ -85,6 +85,7 @@ class OfertaCest
             "nombre_sucursal"=> "Sucursal 1 - Paderia Mitre Modificado",
             "puesto"=> "cajera otro",
             "area"=>"nueva area",
+            "fecha_inicial" => '2018-09-13 09:34:45',
             "fecha_final"=> "",
             "demanda_laboral"=> "Falta dividir responsabilidades",
             "objetivo"=> "Poder dar una oportunidad de trabajo",
@@ -131,6 +132,152 @@ class OfertaCest
         
         $I->seeResponseContainsJson([            
             'message' => 'Se guarda una Oferta'
+        ]);
+        
+        $I->seeResponseCodeIs(200);
+        
+    }
+   
+    public function ListaOferta(ApiTester $I)
+    {
+        $I->wantTo('Lista de ofertas');
+        
+        $I->sendGET('/api/ofertas');
+        
+        $I->seeResponseContainsJson([            
+            "total_filtrado"=> 5,
+            "success"=> true,
+            "resultado"=> [
+                [
+                    "id"=> 1,
+                    "ambiente_trabajoid"=> 1,
+                    "nombre_sucursal"=> "Sucursal 1 - Paderia Mitre Modificado",
+                    "puesto"=> "cajera otro",
+                    "area"=> "nueva area",
+                    "fecha_inicial"=> '2018-10-13 10:34:45',
+                    "fecha_final"=> null,
+                    "demanda_laboral"=> "Falta dividir responsabilidades",
+                    "objetivo"=> "Poder dar una oportunidad de trabajo",
+                    "lugarid"=> 1,
+                    "lugar"=> [
+                        "id"=> 1,
+                        "nombre"=> "",
+                        "barrio"=> "Don bosco",
+                        "calle"=> "Mitre",
+                        "altura"=> "327",
+                        "piso"=> "A",
+                        "depto"=> "",
+                        "escalera"=> "",
+                        "localidadid"=> 1,
+                        "latitud"=> "-123123",
+                        "longitud"=> "321123"
+                    ],
+                    "ambiente_trabajo"=> "Panaderia San Fernando"
+                ],
+                [
+                    "id"=> 2,
+                    "ambiente_trabajoid"=> 1,
+                    "nombre_sucursal"=> "Sucursal Nº 2",
+                    "puesto"=> "cajera",
+                    "area"=> "",
+                    "fecha_inicial"=> '2018-09-10 17:34:45',
+                    "fecha_final"=> null,
+                    "demanda_laboral"=> "falta una cajera",
+                    "objetivo"=> "conseguir personal especificamente para la caja",
+                    "lugarid"=> 2,
+                    "lugar"=> [
+                        "id"=> 2,
+                        "nombre"=> "",
+                        "barrio"=> "Ina Lauquen",
+                        "calle"=> "Saavedra",
+                        "altura"=> "321",
+                        "piso"=> "",
+                        "depto"=> "",
+                        "escalera"=> "",
+                        "localidadid"=> 1,
+                        "latitud"=> "-123321",
+                        "longitud"=> "321321"
+                    ],
+                    "ambiente_trabajo"=> "Panaderia San Fernando"
+                ],
+                [
+                    "id"=> 3,
+                    "ambiente_trabajoid"=> 1,
+                    "nombre_sucursal"=> "Sucursal Nº 2",
+                    "puesto"=> "Limipieza",
+                    "area"=> "",
+                    "fecha_inicial"=> '2018-09-13 09:34:45',
+                    "fecha_final"=> null,
+                    "demanda_laboral"=> "falta mantenimiento en la sucursal",
+                    "objetivo"=> "conseguir personal especificamente para limpieza",
+                    "lugarid"=> 2,
+                    "lugar"=> [
+                        "id"=> 2,
+                        "nombre"=> "",
+                        "barrio"=> "Ina Lauquen",
+                        "calle"=> "Saavedra",
+                        "altura"=> "321",
+                        "piso"=> "",
+                        "depto"=> "",
+                        "escalera"=> "",
+                        "localidadid"=> 1,
+                        "latitud"=> "-123321",
+                        "longitud"=> "321321"
+                    ],
+                    "ambiente_trabajo"=> "Panaderia San Fernando"
+                ],
+                [
+                    "id"=> 4,
+                    "ambiente_trabajoid"=> 2,
+                    "nombre_sucursal"=> "Sucursal Nº 1",
+                    "puesto"=> "Limipieza",
+                    "area"=> "",
+                    "fecha_inicial"=> '2019-09-16 16:03:49',
+                    "fecha_final"=> null,
+                    "demanda_laboral"=> "falta mantenimiento en la sucursal",
+                    "objetivo"=> "conseguir personal especificamente para limpieza",
+                    "lugarid"=> 3,
+                    "lugar"=> [
+                        "id"=> 3,
+                        "nombre"=> "",
+                        "barrio"=> "Don bosco",
+                        "calle"=> "Mitre",
+                        "altura"=> "327",
+                        "piso"=> "2",
+                        "depto"=> "A",
+                        "escalera"=> "",
+                        "localidadid"=> 1,
+                        "latitud"=> "-123123",
+                        "longitud"=> "321123"
+                    ],
+                    "ambiente_trabajo"=> "Panaderia Boomble"
+                ],
+                [
+                    "id"=> 5,
+                    "ambiente_trabajoid"=> 1,
+                    "nombre_sucursal"=> "Sucursal 1 - Paderia Mitre Modificado",
+                    "puesto"=> "cajera otro",
+                    "area"=> "nueva area",
+                    "fecha_final"=> null,
+                    "demanda_laboral"=> "Falta dividir responsabilidades",
+                    "objetivo"=> "Poder dar una oportunidad de trabajo",
+                    "lugarid"=> 1,
+                    "lugar"=> [
+                        "id"=> 1,
+                        "nombre"=> "",
+                        "barrio"=> "Don bosco",
+                        "calle"=> "Mitre",
+                        "altura"=> "327",
+                        "piso"=> "A",
+                        "depto"=> "",
+                        "escalera"=> "",
+                        "localidadid"=> 1,
+                        "latitud"=> "-123123",
+                        "longitud"=> "321123"
+                    ],
+                    "ambiente_trabajo"=> "Panaderia San Fernando"
+                ]
+            ]
         ]);
         
         $I->seeResponseCodeIs(200);
