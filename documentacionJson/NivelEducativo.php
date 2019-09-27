2bc2c21415373278619a411d9ff23c996dd92618
cep11p <cep11p@gmail.com>
2019-09-27 11:19:54 -0300

    TG-293 Se realiza la documentacionJson https://tree.taiga.io/project/cep11p-pril/us/293?no-milestone=1

---
+++ documentacionJson/NivelEducativo.php
@@ -0,0 +1,29 @@
+<?php
+
+
+/**** obtener lista de nivel educativo desde registral (Interoperabilidad)***
+@url ejemplo http://pril.local/api/nivel-educativos
+@Method GET
+[
+    {
+        "id": 1,
+        "nombre": "Pre-Escolar"
+    },
+    {
+        "id": 2,
+        "nombre": "Primario"
+    },
+    {
+        "id": 3,
+        "nombre": "Secundario"
+    },
+    {
+        "id": 4,
+        "nombre": "Terciario"
+    },
+    {
+        "id": 5,
+        "nombre": "Universitario"
+    }
+]
+**/
